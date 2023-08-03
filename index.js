671bd0e834678c3fff363eb91f88d4b0b5b3070a
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2023-08-03 12:12:04 +0530

    improved signup, login, auth with jwt

--- index.js
+++ index.js
@@ -13,144 +13,125 @@ const User = require('./models/user');
 const { auth } = require('./middlewares/auth');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-secretKey=process.env.SECRET
+secretKey = process.env.SECRET
 // database connection
 mongoose.set('strictQuery', false);
 const connectDatabase = async () => {
-    try {
-        const connection = await mongoose.connect(process.env.DATABASE);
-        console.log(`MongoDb connected: ${connection.connection.host}`);
-    }
-    catch (error) {
-        console.log("Error: ", error);
-    }
+  try {
+    const connection = await mongoose.connect(process.env.DATABASE);
+    console.log(`MongoDb connected: ${connection.connection.host}`);
+  }
+  catch (error) {
+    console.log("Error: ", error);
+  }
 }
-// mongoose.connect(db.DATABASE,{ useNewUrlParser: true },function(err){
-//     if(err) console.log(err);
-//     console.log("database is connected");
-// });
 
-
-app.get('/', function (req, res) {
-    res.status(200).send(`Welcome to login , sign-up api`);
+app.get('/', (req, res) => {
+  res.status(200).send(`Welcome to login , sign-up api`);
 });
 
 app.post('/api/register', async (req, res) => {
-    // taking a user
-    const newuser = new User(req.body);
+  // taking a user
+  const newUser = new User(req.body);
 
-    if (newuser.password != newuser.password2) {
-        return res.status(400).json({ message: "password not match" });
+  if (newUser.password != newUser.password2) {
+    return res.status(400).json({ message: "password not match" });
+  }
+
+  try {
+    // Check if a user with the same email or username already exists
+    const existingUser = await User.findOne({ email: req.body.email });
+
+    if (existingUser) {
+      return res.status(409).json({ error: 'User already exists' });
     }
 
-    try {
-        // Check if a user with the same email or username already exists
-        const existingUser = await User.findOne({email: req.body.email});
-    
-        if (existingUser) {
-          return res.status(409).json({ error: 'User already exists' });
-        }
-    
-        // If user does not exist, create a new user
-        const newUser = new User(req.body);
-    newUser.save()
-        // newUser.save((err, user) => {
-        //   if (err) {
-        //     console.error('Error saving user:', err);
-        //     return res.status(500).json({ error: 'Failed to create user' });
-        //   }
-    
-        //   res.status(201).json(user);
-        // });
-      } catch (error) {
-        console.error('Error checking user existence:', error);
-        res.status(500).json({ error: 'Failed to check user existence' });
-      }
-    // User.findOne({email:newuser.email},function(err,user){
-    //     if(user) return res.status(400).json({ auth : false, message :"email exits"});
+    newUser.save();
+    return res.send("Registered successfully")
 
-    //     newuser.save((err,doc)=>{
-    //         if(err) {console.log(err);
-    //             return res.status(400).json({ success : false});}
-    //         res.status(200).json({
-    //             succes:true,
-    //             user : doc
-    //         });
-    //     });
-    // });
+  } catch (error) {
+    console.error('Error checking user existence:', error);
+    res.status(500).json({ error: 'Failed to check user existence' });
+  }
 });
 
 // login user
 app.post('/api/login', async (req, res) => {
-    const { email, password } = req.body;
-  
-    try {
-      // Find the user by email
-      const user = await User.findOne({ email });
-  
-      if (!user) {
-        return res.status(401).json({ error: 'Invalid credentials' });
-      }
-  
-      // Compare passwords
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-  
-      if (!isPasswordValid) {
-        return res.status(401).json({ error: 'Invalid credentials' });
-      }
-  
-      // Generate JWT token
-      const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: '1h' });
-  
-      res.status(200).json({ token });
-    } catch (error) {
-      console.error('Error signing in:', error);
-      res.status(500).json({ error: 'Failed to sign in' });
+  const { email, password } = req.body;
+
+  try {
+    // Find the user by email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid credentials' });
     }
-  });
-  
+
+    // Compare passwords
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: '1h' });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    //console.error('Error signing in:', error);
+    res.status(500).json({ error: 'Failed to sign in' });
+  }
+});
+
 // get logged in user
 app.get('/api/profile', (req, res) => {
-    // Check for the token in the Authorization header
-    const token = req.headers.authorization;
-    if (!token) {
-      return res.status(401).json({ error: 'No token provided' });
-    }
-  
-    // Verify the token and extract the userId
-    jwt.verify(token, secretKey, (err, decoded) => {
-      if (err) {
-        return res.status(401).json({ error: 'Invalid token' });
-      }
-  
-      const userId = decoded.userId;
-      
-      res.status(200).json({ message: 'Protected route accessed successfully', userId });
-    });
-  });
-    
-// app.get('/api/profile', auth, function (req, res) {
-//     res.json({
-//         isAuth: true,
-//         id: req.user._id,
-//         email: req.user.email,
-//         name: req.user.firstname + req.user.lastname
+  // Check for the token in the Authorization header
 
-//     })
-// });
-//logout user
-app.get('/api/logout', function (req, res) {
-    req.user.deleteToken(req.token, (err, user) => {
-        if (err) return res.status(400).send(err);
-        res.sendStatus(200);
-    });
+  const token = req.headers.token;
+  var firstname, lastname, email;
+  //console.log(token);
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  // Verify the token and extract the userId
+  jwt.verify(token, secretKey, async(err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+    const userId = decoded.userId;
+    const user = await User.findOne({ "_id": userId });
+    const userProfile = {
+      'firstName':user.firstname,
+      'lastName':user.lastname,
+      'email':user.email
+    }
+    res.status(200).json({ userProfile });
+  });
+});
+
+
+app.get('/api/logout', async (req, res) => {
+  let token = req.headers.token;
+  logoutUser = await User.findOne({ "token": token });
+  return res.send("200");
+  // const user = User.findByToken(req.headers.token)
+  // user.deleteToken(req.token, (err, user) => {
+  //   if (err) return res.status(400).send(err);
+  //   res.sendStatus(200);
+  // });
+  // req.user.deleteToken(req.token, (err, user) => {
+  //   if (err) return res.status(400).send(err);
+  //   res.sendStatus(200);
+  // });
 
 });
 
 // listening port
 const PORT = process.env.PORT || 5000;
 connectDatabase().then(() => {
-    app.listen(PORT, () => {
-        console.log(`app is live at ${PORT}`);
-    });
+  app.listen(PORT, () => {
+     console.log(`app is live at ${PORT}`);
+  });
 })
--- models/user.js
+++ models/user.js
@@ -36,45 +36,48 @@ const userSchema=mongoose.Schema({
     }
 });
 
-userSchema.methods.generateToken=function(cb){
+userSchema.methods.generateToken=(cb)=>{
     var user =this;
     var token=jwt.sign(user._id.toHexString(),confiq.SECRET);
 
     user.token=token;
-    user.save(function(err,user){
+    user.save((err,user)=>{
         if(err) return cb(err);
         cb(null,user);
     })
 }
-userSchema.statics.findByToken = function (token, cb) {
+userSchema.statics.findByToken =  (token, cb)=> {
     var user = this;
   
-    jwt.verify(token, process.env.SECRET, function (err, decode) {
+    jwt.verify(token, process.env.SECRET, (err, decode) =>{
       if (err) return cb(err);
   
-      user.findOne({ "_id": decode, "token": token }, function (err, user) {
+      user.findOne({ "_id": decode, "token": token },  (err, user)=> {
         if (err) return cb(err);
         cb(null, user);
       });
     });
   };
-userSchema.methods.deleteToken=function(token,cb){
+userSchema.methods.deleteToken=(token,cb)=>{
     var user=this;
+    
+    const logoutUser= user.findOne({ "_id": decode, "token": token });
+    console.log(logoutUser);
 
-    user.update({$unset : {token :1}},function(err,user){
+    logoutUser.update({$unset : {token :1}},(err,user)=>{
         if(err) return cb(err);
         cb(null,user);
     })
 }
 
-userSchema.pre('save',function(next){
+userSchema.pre('save',(next)=>{
     var user=this;
     
     if(user.isModified('password')){
-        bcrypt.genSalt(salt,function(err,salt){
+        bcrypt.genSalt(salt,(err,salt)=>{
             if(err)return next(err);
 
-            bcrypt.hash(user.password,salt,function(err,hash){
+            bcrypt.hash(user.password,salt,(err,hash)=>{
                 if(err) return next(err);
                 user.password=hash;
                 user.password2=hash;
@@ -88,8 +91,8 @@ userSchema.pre('save',function(next){
     }
 });
 
-userSchema.methods.comparepassword=function(password,cb){
-    bcrypt.compare(password,this.password,function(err,isMatch){
+userSchema.methods.comparepassword=(password,cb)=>{
+    bcrypt.compare(password,this.password,(err,isMatch)=>{
         if(err) return cb(next);
         cb(null,isMatch);
     });
